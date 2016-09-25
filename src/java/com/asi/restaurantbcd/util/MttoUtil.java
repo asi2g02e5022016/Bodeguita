2c075a2d093f147e4a5e1219b0e9219fafb080fd
luis_portillo <luis_portillo@ALSVCORLC3X9.alsicorp.milady.local>
2016-09-24 21:19:45 -0600

    Issue #1: Mantenimiento Finalizado (Issue Resolved)

--- src/java/com/asi/restaurantbcd/util/MttoUtil.java
+++ src/java/com/asi/restaurantbcd/util/MttoUtil.java
@@ -6,28 +6,35 @@
 package com.asi.restaurantbcd.util;
 
 import com.asi.restaurantebcd.negocio.base.CrudBDCLocal;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.ValidatorFactory;
 
-/**
- *
- * @author luis_portillo
- */
+
+
 public abstract class MttoUtil<E> {
     
     @PersistenceContext(unitName = "RestaurantBDC-WebPU")
     private EntityManager em;
     @Resource
     private javax.transaction.UserTransaction utx;  
+    
+    @Inject
+    private Conversation conversation;
     
     private List<E> resultList;
     private String jpql;
@@ -39,6 +46,7 @@ public abstract class MttoUtil<E> {
     private Object action;
     
     private FacesContext facesContext;
+
     
     protected final Severity ERROR = FacesMessage.SEVERITY_ERROR;
  
@@ -49,9 +57,6 @@ public abstract class MttoUtil<E> {
     @EJB
     CrudBDCLocal ejbCrud;
     
-    public MttoUtil(){
-        facesContext = FacesContext.getCurrentInstance();
-    }
     
      @SuppressWarnings("unchecked")
 	public List<E> getResultList() {
@@ -112,38 +117,92 @@ public abstract class MttoUtil<E> {
        if (action != null && key != null && "Editar".equals(action.toString())) {
            try {
                System.out.println("Ejecutado loadInstance key = " + key + " ---");
+               this.beginConversation();
                instance = ejbCrud.buscarEntidad(this.getNew().getClass(), key);
            } catch (Exception ex) {
                Logger.getLogger(this.getClass().getName()).log(Level.INFO, null, ex);
            }
-       } else{
-          instance = getNew();
+       } else if(action != null && key != null && "Crear".equals(action.toString())){
+           this.beginConversation();   
+           instance = getNew();
        }
     }
     
    public void save() {
+       facesContext = FacesContext.getCurrentInstance();
        System.out.println("Guardando----");
        if (instance != null) {
            try {
                ejbCrud.persistirEntidad(instance);
                postSave();
            } catch (Exception ex) {
-               Logger.getLogger(this.getClass().getName()).log(Level.INFO, null, ex);
+               Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+               beanValidations();
                addMessage(ERROR, "Error", ex.getMessage());
+               facesContext.validationFailed();
            }
        }
    }
-    
-   @PostConstruct
+   
+   public void delete() {
+              facesContext = FacesContext.getCurrentInstance();
+       System.out.println("Eliminando----");
+       if (instance != null) {
+           try {
+/*      
+        return null;
+    }else{*/
+               ejbCrud.eliminarEntidad(instance);
+               postDelete();
+       
+    //}
+
+           } catch (Exception ex) {
+               Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+               beanValidations();
+               facesContext.validationFailed();
+           }
+       }
+   }
+
+   public void update() {
+              facesContext = FacesContext.getCurrentInstance();
+       System.out.println("Actualizando----");
+       if (instance != null) {
+           try {
+/*      
+        return null;
+    }else{*/postUpdate();
+               ejbCrud.guardarEntidad(instance);
+               
+       
+    //}
+
+           } catch (Exception ex) {
+               Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+               beanValidations();
+               facesContext.validationFailed();
+           }
+       }
+   }
+   
+  
     public void load(){
-         System.out.println("Instance key = " + key + " ---");
+         System.out.println("Instance Before key = " + key + " ---");
+         if(this.getInstance()== null) {
          onLoad();
-         loadInstance();
-         System.out.println("Instance key = " + instance.toString() + " ---");
+         loadInstance();}
+         System.out.println("Instance After key = " + this.getInstance() + " ---");
     };
     
+    public void startNew(){
+            this.beginConversation();
+            this.instance=this.getNew();
+    }
+    
     protected void addMessage(Severity severity, String summary ,String message){
-       facesContext.addMessage(null, new FacesMessage(severity, summary, message));
+        facesContext = FacesContext.getCurrentInstance();
+        facesContext.addMessage(null, new FacesMessage(severity, summary, message));
     };
     
     protected abstract E getNew();
@@ -151,7 +210,11 @@ public abstract class MttoUtil<E> {
     protected abstract void onLoad();
     
     protected abstract void postSave();
+    
+    protected abstract void postDelete();
 
+    protected abstract void postUpdate();
+    
     /**
      * @return the action
      */
@@ -166,6 +229,30 @@ public abstract class MttoUtil<E> {
         this.action = action;
     }
     
+    private void beanValidations(){
+                ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+                 javax.validation.Validator validator = factory.getValidator();
+                 Set<ConstraintViolation<E>> constraintViolations = validator.validate(instance);
+                 if(constraintViolations.size() > 0){
+                     Iterator<ConstraintViolation<E>> iterator = constraintViolations.iterator();
+                     while(iterator.hasNext()){
+                         ConstraintViolation<E> cv = iterator.next();
+                         Logger.getLogger(this.getClass().getName()).log(Level.WARNING,cv.getRootBeanClass().getName()+"."+cv.getPropertyPath() + " " +cv.getMessage());
+                         addMessage(WARNING, "Error", cv.getPropertyPath() + " " +cv.getMessage());
+                            }
+                     }
+    }
     
+    	public void beginConversation() {
+		if (conversation.isTransient()) {
+			conversation.begin();
+		}
+	}
+
+	public void endConversation() {
+		if (!conversation.isTransient()) {
+			conversation.end();
+		}
+	}
 
 }
--- src/java/com/asi/restaurantebcd/controller/mtto/MttoPerfilMB.java
+++ src/java/com/asi/restaurantebcd/controller/mtto/MttoPerfilMB.java
@@ -3,7 +3,8 @@ package com.asi.restaurantebcd.controller.mtto;
    
 import com.asi.restaurantbcd.modelo.Perfil;
 import com.asi.restaurantbcd.util.MttoUtil;
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Named;
 
 
@@ -11,17 +12,18 @@ import javax.inject.Named;
  * @author luis_portillo
  */
 @Named(value = "mttoPerfil")
-@RequestScoped
-public class MttoPerfilMB extends MttoUtil<Perfil> {
+@ConversationScoped
+public class MttoPerfilMB extends MttoUtil<Perfil> implements Serializable{
    
-
+	
+    private static final long serialVersionUID = 1L;
    /**
      * Creates una nueva instancia de MttoPerfilMB
      * Es obligatorio el llamado a setJpql 
      */
     public MttoPerfilMB() {
         super(); //Llama el constructor de la clase padre
-        setJpql("select p from Perfil p"); //Configura el Jpql a ejecutar para el listado de catalogo
+        setJpql("select p from Perfil p order by p.idPerfil desc"); //Configura el Jpql a ejecutar para el listado de catalogo
     }
 
 
@@ -38,15 +40,31 @@ public class MttoPerfilMB extends MttoUtil<Perfil> {
     @Override
     protected void postSave() {
         addMessage(INFO, "Guardado", "Perfil guardado correctamente");
+        this.endConversation();
+    }
+    
+    @Override
+    protected void postUpdate() {
+      addMessage(INFO, "Editado", "Perfil editado correctamente");
+      this.endConversation();
+    }
+    
+    @Override
+    protected void postDelete() {
+        addMessage(INFO, "Eliminado", "Perfil eliminado correctamente");
+        this.endConversation();
     }
 
     /**
      * Retorna una nueva instancia de Perfil
+     * @return 
      */
     @Override
     protected Perfil getNew() {
         return new Perfil();
     }    
+
+
     
 }
 
