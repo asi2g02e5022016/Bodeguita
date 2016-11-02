f8aabe03395cbb91638103d2e89640e0b9c502c0
samaelopez <samaelopez@samaelopez-SVF14213CLB>
2016-11-01 22:04:35 -0600

    Pantallas por hacer

---
+++ src/java/com/asi/restaurantbcd/modelo/Configuracion.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantbcd.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author samaelopez
+ */
+@Entity
+@Table(name = "configuracion")
+@NamedQueries({
+    @NamedQuery(name = "Configuracion.findAll", query = "SELECT c FROM Configuracion c")})
+public class Configuracion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idconfiguracion")
+    private Integer idconfiguracion;
+    @Size(max = 80)
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Size(max = 45)
+    @Column(name = "valor")
+    private String valor;
+
+    public Configuracion() {
+    }
+
+    public Configuracion(Integer idconfiguracion) {
+        this.idconfiguracion = idconfiguracion;
+    }
+
+    public Integer getIdconfiguracion() {
+        return idconfiguracion;
+    }
+
+    public void setIdconfiguracion(Integer idconfiguracion) {
+        this.idconfiguracion = idconfiguracion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idconfiguracion != null ? idconfiguracion.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Configuracion)) {
+            return false;
+        }
+        Configuracion other = (Configuracion) object;
+        if ((this.idconfiguracion == null && other.idconfiguracion != null) || (this.idconfiguracion != null && !this.idconfiguracion.equals(other.idconfiguracion))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.asi.restaurantbcd.modelo.Configuracion[ idconfiguracion=" + idconfiguracion + " ]";
+    }
+    
+}
