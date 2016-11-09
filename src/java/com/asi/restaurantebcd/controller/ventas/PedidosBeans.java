a751a7fc86bc2c6634dc50e24ee389ba06365f7b
samaelopez <samaelopez@samaelopez-SVF14213CLB>
2016-11-09 17:13:59 -0600

    rpt exit

---
+++ src/java/com/asi/restaurantebcd/controller/ventas/PedidosBeans.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantebcd.controller.ventas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+/**
+ *
+ * @author samaelopez
+ */
+@ManagedBean(name = "pedidosBeans")
+@ViewScoped
+public class PedidosBeans {
+
+    /**
+     * Creates a new instance of PedidosBeans
+     */
+    public PedidosBeans() {
+    }
+    
+}
