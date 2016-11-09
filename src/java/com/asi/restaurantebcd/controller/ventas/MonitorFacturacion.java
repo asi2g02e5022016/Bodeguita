ad287d3b2a0cf54108a3f511fa10e3e56ec095b5
samaelopez <samaelopez@samaelopez-SVF14213CLB>
2016-11-09 17:18:12 -0600

    rpt exit

---
+++ src/java/com/asi/restaurantebcd/controller/ventas/MonitorFacturacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantebcd.controller.ventas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+
+/**
+ *
+ * @author samaelopez
+ */
+@ManagedBean(name = "monitorFacturacion")
+@ViewScoped
+public class MonitorFacturacion {
+
+    /**
+     * Creates a new instance of MonitorFacturacion
+     */
+    public MonitorFacturacion() {
+    }
+    
+}
---
+++ src/java/com/asi/restaurantebcd/controller/ventas/RptVentas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantebcd.controller.ventas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author samaelopez
+ */
+@ManagedBean(name = "rptVentas")
+@ViewScoped
+public class RptVentas {
+
+    /**
+     * Creates a new instance of RptVentas
+     */
+    public RptVentas() {
+    }
+    
+}
