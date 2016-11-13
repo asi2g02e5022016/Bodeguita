d7b38c7e921872ace64ac9adcc42f4c0e0c2f018
jaem90 <alastor1990@gmail.com>
2016-11-12 21:46:28 -0600

    #issue Mantenimiento de ETL

---
+++ src/java/com/asi/restaurantbcd/modelo/Tipotarea.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantbcd.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author JAEM
+ */
+@Entity
+@Table(name = "tipotarea")
+@NamedQueries({
+    @NamedQuery(name = "Tipotarea.findAll", query = "SELECT t FROM Tipotarea t")})
+public class Tipotarea implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 5)
+    @Column(nullable = false, length = 5)
+    private String idtipotarea;
+    @Size(max = 255)
+    @Column(length = 255)
+    private String descripcion;
+
+    public Tipotarea() {
+    }
+
+    public Tipotarea(String idtipotarea) {
+        this.idtipotarea = idtipotarea;
+    }
+
+    public String getIdtipotarea() {
+        return idtipotarea;
+    }
+
+    public void setIdtipotarea(String idtipotarea) {
+        this.idtipotarea = idtipotarea;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idtipotarea != null ? idtipotarea.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tipotarea)) {
+            return false;
+        }
+        Tipotarea other = (Tipotarea) object;
+        if ((this.idtipotarea == null && other.idtipotarea != null) || (this.idtipotarea != null && !this.idtipotarea.equals(other.idtipotarea))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.asi.restaurantbcd.modelo.Tipotarea[ idtipotarea=" + idtipotarea + " ]";
+    }
+    
+}
