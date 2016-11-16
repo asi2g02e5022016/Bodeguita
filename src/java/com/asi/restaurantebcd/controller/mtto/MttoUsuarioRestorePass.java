07e73a192cafc65c7638040f2a253551f9bb06e0
Joaquin Sanchez <tec.jesa1989@gmail.com>
2016-11-16 03:08:48 -0600

    nuevas actualizaciones en el usuario. Ademas agrego modificaciones en el mantenimiento proveedores autorizado por Phillips

---
+++ src/java/com/asi/restaurantebcd/controller/mtto/MttoUsuarioRestorePass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.asi.restaurantebcd.controller.mtto;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author "Joaquin Sanchez SA101110"
+ */
+@ManagedBean(name = "mttoUsuarioPass")
+@ViewScoped
+public class MttoUsuarioRestorePass implements Serializable{
+    
+}
